8abba6731377342f7ccfe63874af97abb31aafe7
robinradic <robin@radic.nl>
2015-03-07 02:53:12 +0100

    added initial test case

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Part of the Radic packges.
+ * Licensed under the MIT license.
+ *
+ * @package        docs
+ * @author         Robin Radic
+ * @license        MIT
+ * @copyright  (c) 2011-2015, Robin Radic
+ * @link           http://radic.mit-license.org
+ */
+
+namespace Laradic\Tests\Config;
+
+use Laradic\Dev\Traits\ServiceProviderTestCaseTrait;
+
+
+/**
+ * ServiceProviderTest
+ *
+ * @group support
+ */
+class ServiceProviderTest extends TestCase
+{
+
+    public function testMe()
+    {
+        $this->testMeOk();
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laradic\Tests\Config;
+
+use Laradic\Dev\AbstractTestCase;
+/**
+ * Part of the Radic packges.
+ * Licensed under the MIT license.
+ *
+ * @package     Laradic\Tests\Support
+ * @author      Robin Radic
+ * @license     MIT
+ * @copyright   (c) 2011-2015, Robin Radic
+ * @link        http://radic.mit-license.org
+ *
+ */
+class TestCase extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+}
